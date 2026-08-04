e201f3a78004842f2abbc2e92eed629ccf1130a1
a.belyaev <a.belyaev@tg.dunice.net>
2026-08-04 15:32:21 +0300

    refactor: change table layout

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -41,35 +41,68 @@ export const Table = ({ data }: TableProps) => {
   }, [normalizedDocuments]);
 
   return (
-    <div className={styles['table-container']}>
-      <table>
-        <thead>
-          <tr>
-            {allColumns.map((headItem, headItemIndex) => {
-              if (typeof headItem === 'number') {
-                return (
-                  <TableHead
-                    key={headItem}
-                    item={String(headItem + 1)}
-                    tagsColor={data.tagsForHeader.filter((tag, tagColorIndex) => {
-                      if (headItemIndex - 2 === tagColorIndex) return tag.color;
-                    })}
-                  />
-                );
-              }
-              return <TableHead key={headItem.dataIndex} item={headItem.title} />;
+    <>
+      {/* <div className={styles['table-container']}>
+        <table>
+          <thead>
+            <tr>
+              {allColumns.map((headItem, headItemIndex) => {
+                if (typeof headItem === 'number') {
+                  return (
+                    <TableHead
+                      key={headItem}
+                      item={String(headItem + 1)}
+                      tagsColor={data.tagsForHeader.filter((tag, tagColorIndex) => {
+                        if (headItemIndex - 2 === tagColorIndex) return tag.color;
+                      })}
+                    />
+                  );
+                }
+                return <TableHead key={headItem.dataIndex} item={headItem.title} />;
+              })}
+            </tr>
+          </thead>
+          <tbody>
+            {normalizedDocuments?.map((tableRowItem: NormalisedTableRowItem) => {
+              return <TableRow key={tableRowItem.id} data={tableRowItem} columns={allColumns} />;
             })}
-          </tr>
-        </thead>
-        <tbody>
-          {normalizedDocuments?.map((tableRowItem: NormalisedTableRowItem) => {
-            return <TableRow key={tableRowItem.id} data={tableRowItem} columns={allColumns} />;
-          })}
-        </tbody>
-        <tfoot>
-          <TableFooter allExcerptSums={allExcerptSums} allTagsSum={allTagsSum} columns={allColumns} />
-        </tfoot>
-      </table>
-    </div>
+          </tbody>
+          <tfoot>
+            <TableFooter allExcerptSums={allExcerptSums} allTagsSum={allTagsSum} columns={allColumns} />
+          </tfoot>
+        </table>
+      </div> */}
+      <div className={styles['table-container']}>
+        <div className={styles['table']}>
+          <div className={styles['table-header']}>
+            <div className={styles['header-left']}>
+              {STATIC_LEFT_COLUMNS.map((headItem) => {
+                return <TableHead key={headItem.dataIndex} item={headItem.title} />;
+              })}
+            </div>
+            <div className={styles['header-mid']}>
+              {data.tagsForHeader.map((tag) => (
+                <TableHead key={tag.order} item={String(tag.order + 1)} tagColor={tag.color} />
+              ))}
+            </div>
+            <div className={styles['header-right']}>
+              {STATIC_RIGHT_COLUMNS.map((headItem) => {
+                return <TableHead key={headItem.dataIndex} item={headItem.title} />;
+              })}
+            </div>
+          </div>
+          <div className={styles['table-viewport']}>
+            <div className={styles['table-body']}>
+              {normalizedDocuments?.map((tableRowItem: NormalisedTableRowItem) => {
+                return <TableRow key={tableRowItem.id} data={tableRowItem} columns={allColumns} />;
+              })}
+            </div>
+          </div>
+          <div className={styles['table-footer']}>
+            <TableFooter allExcerptSums={allExcerptSums} allTagsSum={allTagsSum} columns={allColumns} />
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
--- src/components/Table/components/TableHead/TableHead.tsx
+++ src/components/Table/components/TableHead/TableHead.tsx
@@ -1,15 +1,14 @@
-import type { TagsForHeader } from '../../types';
 import styles from './TableHead.module.css';
 
 type TableHeadProps = {
   item: string;
-  tagsColor?: TagsForHeader[];
+  tagColor?: string;
 };
 
 export const TableHead = (props: TableHeadProps) => {
   return (
-    <td title={props.item} className={`${styles['head-td']} ${Number(props.item) ? styles['tag-head-td'] : ''}`}>
-      <div style={{ backgroundColor: props.tagsColor?.[0].color }}>{props.item}</div>
-    </td>
+    <div title={props.item} className={`${styles['head-td']} ${Number(props.item) ? styles['tag-head-td'] : ''}`}>
+      <div style={{ backgroundColor: props.tagColor }}>{props.item}</div>
+    </div>
   );
 };
